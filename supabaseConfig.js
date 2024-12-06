6829eafec432bd59973ac5a23fb9a3b766c7c1a9
Hridoy Ahmed <99856822+hrid0yyy@users.noreply.github.com>
2024-12-06 22:08:55 +0600

    hide api key and url of supaBase

--- supabaseConfig.js
+++ supabaseConfig.js
@@ -1,9 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://lixgbffpefinudzrltwi.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpeGdiZmZwZWZpbnVkenJsdHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzMTg3MzIsImV4cCI6MjA0ODg5NDczMn0.VA3Ap-yrn6lCexpGMxdP3H7Q_H7b5y7sytF75116Los";
+const SUPABASE_URL = URL;
+const SUPABASE_KEY = KEY;
 
 // Initialize Supabase with AsyncStorage
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
