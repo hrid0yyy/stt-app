5cf230e8918ce8b4268053e2cd1745e9b69ee773
mehrin <mehrin@gamil.com>
2024-12-06 20:10:56 +0600

    cart and category ui

---
+++ app/(app)/community.tsx
@@ -0,0 +1,14 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+export default function community() {
+  return (
+    <View>
+      <Text>Community</Text>
+    </View>
+  );
+}
